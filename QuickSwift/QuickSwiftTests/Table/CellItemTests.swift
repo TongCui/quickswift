aa39f354890778e4d3f7f0dd006c7a7105eaa90d
Tong Cui <tcui@appannie.com>
2018-04-20 15:38:23 +0800

    [*]update ut

--- QuickSwift/QuickSwiftTests/Table/CellItemTests.swift
+++ QuickSwift/QuickSwiftTests/Table/CellItemTests.swift
@@ -31,11 +31,5 @@ class CellItemTests: XCTestCase {
 
     }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
 
 }
